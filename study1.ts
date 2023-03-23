b575691b60136c919724ca0be8154e8b729ff692
Cynthiar1806 <cynthiar@lumel.com>
2023-03-23 14:48:20 +0530

    First Commit

---
+++ study1.ts
@@ -0,0 +1,12 @@
+const disp = (phrase) => {
+    console.log(`**${phrase}**`)
+}
+
+const hello = (name) => {
+    let displayName = `My name is ${name}`;
+    disp(displayName);
+}
+
+hello('lala');
+
+
